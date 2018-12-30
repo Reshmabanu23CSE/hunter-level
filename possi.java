2ec26d12dc5740a6f2471ef6c22e02928299e0a6
Reshmabanu23 <36692753+Reshmabanu23CSE@users.noreply.github.com>
2018-12-30 13:19:22 +0530

    Create possi.java

---
+++ possi.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Possi{
+public static void main(String[] args)
+{
+	Scanner s=new Scanner(System.in);
+
+	int n=s.nextInt();
+	int[] a=new int[n];
+	for(int i=0;i<n;i++)
+	{
+		a[i]=s.nextInt();
+	}
+	for(int i=0;i<n;i++)
+	{
+		for(int j=i+1;j<n;j++)
+		{
+			for(int k=j+1;k<n;k++)
+			{
+				if(a[i]+a[j]==a[k])
+				{
+					System.out.println(a[i]+" "+a[j]+" "+a[k]);
+				}
+			}
+		}
+	}
+	
+}
+}
